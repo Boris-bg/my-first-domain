ef0deab27bbf3b67a0e3824fc664c6d6cef4d19c
Boris-bg <96040310+Boris-bg@users.noreply.github.com>
2022-01-03 20:00:37 +0200

    Add files via upload

---
+++ java.js
@@ -0,0 +1,7 @@
+function PC() {
+    document.getElementById("demo").innerHTML = "TheClashers за 5% - от периферия и TheClashersPC за 3% - от лаптопи и компютри (промокодовете важат и за намалени продукти)";
+}
+
+function laptop() {
+    document.getElementById("demo").innerHTML = "VENOM за 3% от компютърни компоненти и 6% от периферия; <br> PITKA за 3% от компютърни компоненти и 6% от периферия; <br> DRIFT за 3% от компютърни компоненти и 6% от периферия";
+}
